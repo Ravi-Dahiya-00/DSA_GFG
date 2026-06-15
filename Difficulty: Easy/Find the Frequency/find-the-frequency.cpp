a41ed92eabbae236816f72ffc13f0402522d1468
Ravi Yadav <ravidahiya9350@gmail.com>
2026-06-15 23:59:19 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Find the Frequency/find-the-frequency.cpp
@@ -0,0 +1,16 @@
+/*Function to find frequency of x
+ * x : element whose frequency is to be found
+ * arr : input vector
+ */
+class Solution {
+  public:
+    int findFrequency(vector<int> arr, int x) {
+        unordered_map<int,int> mpp;
+        
+        for(int i=0;i<arr.size();i++){
+            mpp[arr[i]]++;
+        }
+        
+        return mpp[x];
+    }
+};
